7340728a37db0e47a1b0047c2fd3272179f05514
Elimasrojas <elimasrojas@gmail.com>
2023-02-26 19:36:27 -0500

    Author CRUD Operations

--- src/main/java/com/elr/elr/dao/AuthorDaoImpl.java
+++ src/main/java/com/elr/elr/dao/AuthorDaoImpl.java
@@ -1,13 +1,22 @@
 package com.elr.elr.dao;
 
 import com.elr.elr.domain.Author;
+import com.elr.elr.repositories.AuthorRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorDaoImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
     @Override
     public Author getById(Long id) {
-        return null;
+        return authorRepository.getById(id);
     }
 
     @Override
@@ -17,16 +26,23 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        return authorRepository.save(author);
     }
 
+    @Transactional
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        Author foundAuthor = authorRepository.getById(author.getId());
+        //Author foundAuthor = authorRepository.getReferenceById(author.getId());
+        foundAuthor.setFirstName(author.getFirstName());
+        foundAuthor.setLastName(author.getLastName());
+        return  authorRepository.save(foundAuthor);
+
+
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-
+        authorRepository.deleteById(id);
     }
 }
--- src/main/java/com/elr/elr/repositories/AuthorRepository.java
+++ src/main/java/com/elr/elr/repositories/AuthorRepository.java
@@ -5,4 +5,5 @@ import com.elr.elr.domain.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
 }
