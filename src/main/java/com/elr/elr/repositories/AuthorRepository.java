284eb7a317f11a9dd4424618e31be02160217f8a
Elimasrojas <elimasrojas@gmail.com>
2023-02-26 20:04:05 -0500

    Query Methods

--- src/main/java/com/elr/elr/repositories/AuthorRepository.java
+++ src/main/java/com/elr/elr/repositories/AuthorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
+            Author findAuthorByFirstNameAndLastName(String firstName, String lastName);
 }
