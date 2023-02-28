cedec4cc1025944fe0e74994d2506adf380169aa
Elimasrojas <elimasrojas@gmail.com>
2023-02-27 20:48:59 -0500

    104. Stream Query Results

--- src/main/java/com/elr/elr/repositories/BookRepository.java
+++ src/main/java/com/elr/elr/repositories/BookRepository.java
@@ -5,6 +5,8 @@ import com.elr.elr.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.stream.Stream;
+
 import org.springframework.lang.Nullable;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -14,4 +16,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Nullable
     Book getByTitle(@Nullable String title);
+
+    Stream<Book> findAllByTitleNotNull();
 }
--- src/test/java/com/elr/elr/BookRepositoryTest.java
+++ src/test/java/com/elr/elr/BookRepositoryTest.java
@@ -2,6 +2,7 @@ package com.elr.elr;
 
 import com.elr.elr.domain.Book;
 import com.elr.elr.repositories.BookRepository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -10,6 +11,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -21,6 +25,17 @@ public class BookRepositoryTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    @Test
+    void testBookStream() {
+        AtomicInteger count = new AtomicInteger();
+
+        bookRepository.findAllByTitleNotNull().forEach(book -> {
+            count.incrementAndGet();
+        });
+
+        assertThat(count.get()).isGreaterThan(4);
+    }
 
     @Test
     void testEmptyResultException() {
